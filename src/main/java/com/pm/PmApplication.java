f380a2003691a5cd17be12e8e5a4af4ffdb32101
THE0321 <89115975+THE0321@users.noreply.github.com>
2024-04-29 09:35:58 +0900

    스타일 작업 진행중

--- src/main/java/com/pm/PmApplication.java
+++ src/main/java/com/pm/PmApplication.java
@@ -2,12 +2,22 @@ package com.pm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
+//@EnableJpaAuditing
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PmApplication.class, args);
     }
 
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
