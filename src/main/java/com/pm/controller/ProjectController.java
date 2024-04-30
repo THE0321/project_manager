94550a789613b45278a7ec40fe5a3d480e0dbe0a
THE0321 <89115975+THE0321@users.noreply.github.com>
2024-04-30 13:56:20 +0900

    스타일 작업 완료

--- src/main/java/com/pm/controller/ProjectController.java
+++ src/main/java/com/pm/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.pm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,10 @@ public class ProjectController {
     @GetMapping(value = {"/", ""})
     public String list() {
         return "/project/list";
+    }
+
+    @GetMapping(value = {"/detail", "/detail/{idx}"})
+    public String detail(@PathVariable(required = false) Integer idx) {
+        return "/project/detail";
     }
 }
